cd8462154a9720bf7dfd6b69dcba0d3a4ec7e740
sunsun <szl843563438@163.com>
2025-09-26 17:07:35 +0800

    更新：新增了weditor的配置版本

--- src/config/coordinates.py
+++ src/config/coordinates.py
@@ -22,3 +22,14 @@ click_position_PVE = (0.536, 0.342)
 
 # 取消按钮坐标
 # click_position_cancel = (0.25, 0.7)
+
+# 启动隐私弹窗resourceId合集
+sdk_privacy_title = "com.duole.wuziqihd:id/dl_sdk_privacy_title" # 启动隐私弹窗标题resourceId
+sdk_privacy_disagree = "com.duole.wuziqihd:id/dl_sdk_privacy_disagree" # 启动隐私弹窗拒绝按钮resourceId
+sdk_privacy_agree = "com.duole.wuziqihd:id/dl_sdk_privacy_agree" # 启动隐私弹窗同意按钮resourceId
+# 登录大厅按钮resourceId合集
+common_login_button = "com.duole.wuziqihd:id/dl_sdk_acc_common_login" # 登录大厅按钮resourceId
+wechat_login_button = "com.duole.wuziqihd:id/dl_sdk_acc_wechat_login" # 微信登录按钮resourceId
+login_service_button = "com.duole.wuziqihd:id/dl_sdk_acc_login_service" # 人工客服按钮resourceId
+login_upload_button = "com.duole.wuziqihd:id/dl_sdk_acc_login_upload_log" # 上传日志按钮resourceId
+login_age_button = "com.duole.wuziqihd:id/dl_sdk_acc_login_suitable_age" # 适龄提醒按钮resourceId
